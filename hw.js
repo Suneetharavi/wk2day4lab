f017dbd2ba7f763c379d38d429dbb90c2702e6ce
Ravikumar Thota <suneetha.gopu3@gmailcom>
2023-10-18 12:05:20 -0400

    Homeworkfinalcopy

--- hw.js
+++ hw.js
@@ -1,10 +1,10 @@
-// 1.   Write an function that contains an if/else statement for the following requirements:
-// - If student gets 80 or higher: console log  You did a good job
-// - If students get 70 or above: console log Keep trying
-// - If students get 60 or above: console log ehhhh
-// - If students get 55 or above: console log Not to good
-// // - Any grade lower than 55 is Bad Grade But You'll Get Em Next Time
-
+// // 1.   Write an function that contains an if/else statement for the following requirements:
+// // - If student gets 80 or higher: console log  You did a good job
+// // - If students get 70 or above: console log Keep trying
+// // - If students get 60 or above: console log ehhhh
+// // - If students get 55 or above: console log Not to good
+// // // - Any grade lower than 55 is Bad Grade But You'll Get Em Next Time
+console.log("\nProgram 1 output \n")
 function score(grade)
 {
     if(grade>=80){
@@ -20,7 +20,8 @@ function score(grade)
       }
 score(99);
 
-// 2.   Write a function that prints out multiples of 10 up to a given input (argument)
+console.log("\nProgram 2 output \n")
+// // 2.   Write a function that prints out multiples of 10 up to a given input (argument)
 
 function mul(num)
 {
@@ -32,34 +33,35 @@ function mul(num)
         
     }
 }
-mul(20)
+mul(85)
 
-
-// 3.   Write a function that takes bill amount and item price and says how many quarters they'd get in return
+console.log("\nProgram 3 output \n")
+// // 3.   Write a function that takes bill amount and item price and says how many quarters they'd get in return
 
 function main(bill,paid)
 {
-    let i=0
-    let quarters=0
-    let change=paid-bill;
-    while(i<change)
+    let k=0
+    let quarts=0
+    let chnge=paid-bill;
+    console.log("The Change need to be paid is:"+chnge)
+    while(k<chnge)
     {
-        quarters+=4
-        i++
+        quarts+=1
+        k=k+0.25
     }
-    console.log(quarters +"quarters")
+    console.log(quarts +"quarters")
 
 }
-main(23,24)
+main(22.75,24)
 
+console.log("\nProgram 4 output\n")
+// //4.    Write a function that prints out how many bills you would recieve in change after paying a certain amount.  
+// // Return in as large denominations as possible. Parameters should be amount paind and amount cost
 
-//4.    Write a function that prints out how many bills you would recieve in change after paying a certain amount.  
-// Return in as large denominations as possible. Parameters should be amount paind and amount cost
-
-function main(cost,paid)
+function mainbill(cost,paid)
 {
    
-    let n=paid-cost;
+    let n=(paid-cost).toFixed(2);
    
     console.log("The Change need to be paid:" +n+'\n')
         hundredd = Math.floor(n/100)
@@ -93,23 +95,23 @@ function main(cost,paid)
         ni = Math.floor(n/0.05)
 
         n %= 0.05
-console.log(n)
-        console.log(n/0.01)
-        c = Math.ceil(n/0.01)
+
+      
+        c = Math.floor(n/0.01)
        
 
         console.log(hundredd +"Hundreds Dollars \n"+ fiftyd+"FiftyDollars\n"+  twentyd +"TwentyDoalls\n"+  tend+"Ten Dollars\n"+fived+"Five Dollars\n"+
         oned+"One Dallars\n"+halfd+"Half Dollars\n"+q +"Quarters\n " + d +"Dimes\n " + ni + "Nickels\n" + c+"Cents\n")
 }
-main(380.90,500)
+mainbill(380.99,500)
 
 
-
-// 5.   Write a program that uses console.log to print all the numbers from 1 to 100 with two exceptions. 
-// For numbers divisible by 3, print "Fizz" instead of the number,
-//  and for numbers divisible by 5 (and not 3), 
-// print "Buzz" instead. When you have that working, modify your program to print "FizzBuzz", 
-// for numbers that are divisible by both 3 and 5 (and still print "Fizz" or "Buzz" for numbers divisible by only one of those).
+console.log("\nProgram 5 output\n")
+// // 5.   Write a program that uses console.log to print all the numbers from 1 to 100 with two exceptions. 
+// // For numbers divisible by 3, print "Fizz" instead of the number,
+// //  and for numbers divisible by 5 (and not 3), 
+// // print "Buzz" instead. When you have that working, modify your program to print "FizzBuzz", 
+// // for numbers that are divisible by both 3 and 5 (and still print "Fizz" or "Buzz" for numbers divisible by only one of those).
 
 
 for ( let i= 1; i< 101; i ++ ) {
@@ -130,7 +132,3 @@ for ( let i= 1; i< 101; i ++ ) {
       console.log( i );
   } 
   
-
-
-
- 
